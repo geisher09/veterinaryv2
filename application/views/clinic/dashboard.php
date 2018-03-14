e00250f850902ecf7963a3b1b611b4afaadebd8d
geisher09 <geisherbernabe@gmail.com>
2018-03-15 00:06:49 +0800

    acc settings and add user UI

--- application/views/clinic/dashboard.php
+++ application/views/clinic/dashboard.php
@@ -98,13 +98,18 @@
                             </div>
                         </form>
                         <ul class="navbar-nav">
-                            <li class="nav-item">
-                                <a class="nav-link" href="#pablo">
-                                    <i class="now-ui-icons media-2_sound-wave"></i>
-                                    <p>
-                                        <span class="d-lg-none d-md-block">Stats</span>
-                                    </p>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <i class="now-ui-icons ui-2_settings-90"></i>
                                 </a>
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink1">
+                                    <a class="dropdown-item" href="<?php echo base_url('vetclinic/accountsettings'); ?>">
+                                        Account Settings
+                                     </a>
+                                    <a class="dropdown-item" href="<?php echo base_url('vetclinic/adduser'); ?>">
+                                        Add New User
+                                     </a>
+                                </div>
                             </li>
                             <li class="nav-item dropdown">
                                 <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
